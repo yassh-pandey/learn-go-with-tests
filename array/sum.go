35525171867e505496f37904f7e91a819370e99c
Yash_Science <yashpandey.science@gmail.com>
2020-12-06 02:23:57 +0530

    ADD SumAll and SumTails functions

--- array/sum.go
+++ array/sum.go
@@ -8,3 +8,24 @@ func Sum(numbers []float64) float64 {
 	}
 	return sum
 }
+
+// SumAll takes a slice of slice of floating point numbers and
+// returns a slice containing the sum of individual slice elements
+func SumAll(listOfNumberList [][]float64) (listSum []float64) {
+	for _, list := range listOfNumberList {
+		listSum = append(listSum, Sum(list))
+	}
+	return
+}
+
+// SumAllTails calculates sum of all elements of a slice except it's head element
+func SumAllTails(numbersToSum ...[]float64) (sumList []float64) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) <= 1 {
+			sumList = append(sumList, 0)
+		} else {
+			sumList = append(sumList, Sum(numbers[1:]))
+		}
+	}
+	return
+}
--- array/sum_test.go
+++ array/sum_test.go
@@ -1,6 +1,9 @@
 package array
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	got := Sum([]float64{17, 99, 133})
@@ -9,4 +12,41 @@ func TestSum(t *testing.T) {
 	if got != expect {
 		t.Errorf("Expected sum: %.2f but got: %.2f", expect, got)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	arr := [][]float64{{1, 55, 9}, {12, 45, -4}, {-83, 0, 83}}
+	got := SumAll(arr)
+	expect := []float64{65, 53, 0}
+	if len(got) == len(expect) {
+		for i, v := range got {
+			if expect[i] != v {
+				t.Errorf("Expected: %v, got this: %v for input = %v", expect, got, arr)
+			}
+		}
+	} else {
+		t.Errorf("Expected: %v, got this: %v for input = %v", expect, got, arr)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	assertCorrectness := func(t *testing.T, got, expect []float64) {
+		t.Helper()
+		if !reflect.DeepEqual(got, expect) {
+			t.Errorf("Expected: %v, got this: %v", expect, got)
+		}
+	}
+
+	t.Run("Tail sum of non empty slices", func(t *testing.T) {
+		got := SumAllTails([]float64{12, 44, 9}, []float64{-9, 9, 0})
+		expect := []float64{53, 9}
+		assertCorrectness(t, got, expect)
+	})
+
+	t.Run("Tail sum for empty slices", func(t *testing.T) {
+		got := SumAllTails([]float64{}, []float64{-999})
+		expect := []float64{0, 0}
+		assertCorrectness(t, got, expect)
+	})
 }
